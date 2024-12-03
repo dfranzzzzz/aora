53fecde99318e69a70b2b5a6aa045cda2d8532b5
Franz Decinal <dfranzzzzz05@gmail.com>
2024-12-04 06:20:31 +0800

    Updated home screen

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -8,39 +8,44 @@ import Trending from "@/components/Trending";
 import VideoCard from "@/components/VideoCard";
 
 import { images } from "@/assets";
-import { getAllPosts } from "@/lib/appwrite";
+import { getAllPosts, getLatestPosts } from "@/lib/appwrite";
 import useAppwrite from "@/lib/useAppwrite";
 
 const Home = () => {
   const { data: posts, refetch } = useAppwrite(getAllPosts);
+  const { data: latestPosts } = useAppwrite(getLatestPosts);
 
   const [refreshing, setRefreshing] = useState(false);
 
   const onRefresh = async () => {
     setRefreshing(true);
-    await refetch;
+    await refetch();
     setRefreshing(false);
   };
 
   return (
-    <SafeAreaView className="bg-primary h-full">
+    <SafeAreaView className="bg-primary">
       <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => <VideoCard video={item} />}
         ListHeaderComponent={() => (
-          <View className="my-6 px-4 space-y-6">
-            <View className="justify-between items-start flex-row mb-6">
+          <View className="flex my-6 px-4 space-y-6">
+            <View className="flex justify-between items-start flex-row mb-6">
               <View>
                 <Text className="font-pmedium text-sm text-gray-100">
-                  Welcome back
+                  Welcome Back
                 </Text>
                 <Text className="text-2xl font-psemibold text-white">
                   dfranzzzzz
                 </Text>
               </View>
+
               <View className="mt-1.5">
                 <Image
                   source={images.logoSmall}
-                  resizeMode="contain"
                   className="w-9 h-10"
+                  resizeMode="contain"
                 />
               </View>
             </View>
@@ -52,17 +57,14 @@ const Home = () => {
                 Latest Videos
               </Text>
 
-              <Trending posts={[{ id: 1 }, { id: 2 }, { id: 3 }] ?? []} />
+              <Trending posts={latestPosts ?? []} />
             </View>
           </View>
         )}
-        data={posts}
-        keyExtractor={(item) => item.$id}
-        renderItem={({ item }) => <VideoCard video={item} />}
         ListEmptyComponent={() => (
           <EmptyState
-            title="No videos found"
-            subtitle="Be the first one to upload a video"
+            title="No Videos Found"
+            subtitle="No videos created yet"
           />
         )}
         refreshControl={
