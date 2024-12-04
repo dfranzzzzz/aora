7e067beb45d7a667e3d04d79548e56e285d3cdba
Franz Decinal <dfranzzzzz05@gmail.com>
2024-12-04 23:52:53 +0800

    Updated search screen and SearchInput component

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
-import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Alert, Image, TextInput, TouchableOpacity, View } from "react-native";
+import { usePathname, router } from "expo-router";
 
 import { icons } from "@/assets";
 
-const SearchInput = () => {
-  const [query, setQuery] = useState();
+interface SearchInputProps {
+  initialQuery?: string;
+}
+
+const SearchInput = ({ initialQuery }: SearchInputProps) => {
+  const pathname = usePathname();
+  const [query, setQuery] = useState(initialQuery || "");
 
   return (
     <View className="flex flex-row items-center space-x-4 w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200 focus:border-secondary">
@@ -13,10 +19,21 @@ const SearchInput = () => {
         value={query}
         placeholder="Search a video topic"
         placeholderTextColor="#CDCDE0"
-        onChangeText={() => console.log("")}
+        onChangeText={(e) => setQuery(e)}
       />
 
-      <TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          if (query === "")
+            return Alert.alert(
+              "Missing Query",
+              "Please input something to search results across database"
+            );
+
+          if (pathname.startsWith("/search")) router.setParams({ query });
+          else router.push(`/search/${query}`);
+        }}
+      >
         <Image source={icons.search} resizeMode="contain" className="w-5 h-5" />
       </TouchableOpacity>
     </View>
